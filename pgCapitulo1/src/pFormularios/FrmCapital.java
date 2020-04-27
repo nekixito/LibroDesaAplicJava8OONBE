3e8cb0e38359a00cbf7260de19444db0d19ec690
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2020-04-27 07:45:48 -0500

    Actualizacion del nombre del archivo FrmCapital.java

--- pgCapitulo1/src/pFormularios/FrmCapital.java
+++ pgCapitulo1/src/pFormularios/FrmCapital.java
@@ -9,12 +9,12 @@ package pFormularios;
  *
  * @author Multi
  */
-public class frmCapital extends javax.swing.JFrame {
+public class FrmCapital extends javax.swing.JFrame {
 
     /**
      * Creates new form frmCapital
      */
-    public frmCapital() {
+    public FrmCapital() {
         initComponents();
     }
 
@@ -261,20 +261,21 @@ public class frmCapital extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frmCapital.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCapital.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frmCapital.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCapital.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frmCapital.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCapital.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frmCapital.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmCapital.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frmCapital().setVisible(true);
+                new FrmCapital().setVisible(true);
             }
         });
     }
